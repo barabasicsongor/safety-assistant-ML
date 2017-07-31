20dffece2bc774cc51d7d4fe64c3df71717ef16e
Csongor Barabasi <barabasi.csongi96@gmail.com>
2017-07-31 10:37:33 +0100

    AWS Deployment updates

--- flask-ml/web_req.py
+++ flask-ml/web_req.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-url = 'http://0.0.0.0:5000/api'
+url = 'http://safetyassistant.eu-west-2.elasticbeanstalk.com/api'
 data = json.dumps({'day': 'Monday', 'place': 'California St, San Francisco, CA'})
 r = requests.post(url, data)
 print(r.json())
